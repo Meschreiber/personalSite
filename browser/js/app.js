d075f8e752d978eefccc2a31862898e2720de88d
Maria Schreiber <meschreiber3@gmail.com>
2017-07-19 19:02:33 -0400

    Add firebase project api key

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '../stylesheets/index.scss';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+
+const App = () => (
+  <MuiThemeProvider>
+    <div>Good morning, world.  I'm working on it.</div>
+  </MuiThemeProvider>
+);
 
 
 ReactDOM.render(
-  <div>Good morning, world.</div>,
+  <App />,
   document.getElementById('app')
 )
