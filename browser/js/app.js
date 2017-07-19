4acbfd2f84df4a68562f4abc9d1397c366fd6e1f
Maria Schreiber <meschreiber3@gmail.com>
2017-07-19 13:49:29 -0400

    Initialize boilerplate

---
+++ browser/js/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../stylesheets/index.scss';
+
+
+ReactDOM.render(
+  <div>Good morning, world.</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './browser/js/app.js', // assumes your entry point is the index.js in the root of your project folder
+  output: {
+    path: __dirname,
+    filename: './bundle.js' // assumes your bundle.js will also be in the root of your project folder
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
