8f67f325fb6469066cbefc860185c4d9a0e89520
Maria Schreiber <meschreiber3@gmail.com>
2017-07-20 17:40:32 -0400

    Extra clean up

--- browser/js/components/Menu.jsx
+++ browser/js/components/Menu.jsx
@@ -5,7 +5,7 @@ const menuInfo = [
   {
     id: 0,
     title: 'My Background',
-    imageSrc: '../../images/ibbio.png',
+    imageSrc: '../../images/iBBio.png',
     imageAlt: 'students',
     blurb: 'teacher => learner => coder',
     link: 'background'
