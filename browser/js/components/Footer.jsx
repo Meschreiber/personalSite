7435a7097a195543d421b45df493fff8ed3cc4c5
Maria Schreiber <meschreiber3@gmail.com>
2017-07-20 12:09:04 -0400

    Create footer

---
+++ browser/js/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer>
+    <p> © 2017 | Handcoded with love </p>
+    <div className="icons">
+      <a className="icon" href="https://github.com/meschreiber" target="_blank">
+        <i className="fa fa-file-pdf-o fa-2x" />
+      </a>
+      <a className="icon" href="https://github.com/meschreiber" target="_blank">
+        <i className="fa fa-github fa-2x" />
+      </a>
+      <a className="icon" href="https://www.linkedin.com/in/maria-elisabeth-schreiber/" target="_blank">
+        <i className="fa fa-linkedin fa-2x" />
+      </a>
+      <a className="icon" href="https://medium.com/@meschreiber3" target="_blank">
+        <i className="fa fa-medium fa-2x" />
+      </a>
+      <a className="icon" href="mailto:meschreiber3@gmail.com">
+        <i className="fa fa-envelope-o fa-2x" />
+      </a>
+    </div>
+
+  </footer>
+)
+
+export default Footer
--- browser/js/components/Main.jsx
+++ browser/js/components/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Topbar from './Topbar'
+import Footer from './Footer'
 
 const Main = () => (
   <div>
     <Topbar />
+    <img src="../../images/rubberducks.jpg" alt="Duckies" style={{width: 100 + '%'}} />
+    <Footer />
   </div>
 )
 
