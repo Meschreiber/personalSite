a45593b787ca0e859cf52bf14396a827a0dd4389
Maria Schreiber <meschreiber3@gmail.com>
2017-07-21 11:38:05 -0400

    Fix Navbar to the top

--- browser/js/components/Topbar.jsx
+++ browser/js/components/Topbar.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import {
-  fullblack, grey900
-} from 'material-ui/styles/colors'
 
 
 const Topbar = () => (
-    <AppBar
-      className="head"
-      title="Maria Schreiber"
-      style={{backgroundColor: grey900, fontFamily: 'PT Sans'}}
-      iconElementLeft={<span/>}
-      iconStyleLeft={{textAlign: 'center'}}
-    />
+  <div className="topBar">
+    <div className="container">
+      <p className = "topName">Maria Schreiber || Software Engineer </p>
+      <ul>
+        <li className="topbar">
+          <a className="topIcon" href="../../Maria_Schreiber_Resume.pdf" target="_blank">
+            <i className="fa fa-file-pdf-o fa-2x" />
+          </a>
+        </li>
+        <li className="topbar">
+          <a className="topIcon" href="https://github.com/meschreiber" target="_blank">
+            <i className="fa fa-github fa-2x" />
+          </a>
+        </li>
+        <li className="topbar">
+          <a className="topIcon" href="https://www.linkedin.com/in/maria-elisabeth-schreiber/" target="_blank">
+            <i className="fa fa-linkedin fa-2x" />
+          </a>
+        </li>
+        <li className="topbar">
+          <a className="topIcon" href="https://medium.com/@meschreiber3" target="_blank">
+            <i className="fa fa-medium fa-2x" />
+          </a>
+        </li>
+        <li className="topbar">
+          <a className="topIcon" href="mailto:meschreiber3@gmail.com">
+            <i className="fa fa-envelope-o fa-2x" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 )
 
 export default Topbar
