4923cee93bae18714995da58591e74a8c67e41c4
Maria Schreiber <meschreiber3@gmail.com>
2017-07-24 11:58:42 -0400

    Refactor to bootstrap navbar

--- browser/js/components/Topbar.jsx
+++ browser/js/components/Topbar.jsx
@@ -2,33 +2,50 @@ import React from 'react'
 
 
 const Topbar = () => (
-  <nav className="topBar">
-    <div className="container">
-      <p className = "topName">Maria Schreiber || Software Engineer </p>
-      <ul>
-        <li className="topbar">
-          <a className="topIcon" href="https://github.com/meschreiber" target="_blank">
-            <i className="fa fa-github fa-2x" />
-          </a>
-        </li>
-        <li className="topbar">
-          <a className="topIcon" href="https://www.linkedin.com/in/maria-elisabeth-schreiber/" target="_blank">
-            <i className="fa fa-linkedin fa-2x" />
-          </a>
-        </li>
-        <li className="topbar">
-          <a className="topIcon" href="https://medium.com/@meschreiber3" target="_blank">
-            <i className="fa fa-medium fa-2x" />
-          </a>
-        </li>
-        <li className="topbar">
-          <a className="topIcon" href="mailto:meschreiber3@gmail.com">
-            <i className="fa fa-envelope-o fa-2x" />
-          </a>
-        </li>
-      </ul>
+  <nav className="navbar navbar-inverse topBar">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a className="navbar-brand" href="#">Maria Schreiber || Progammer</a>
+      </div>
+      <div className="collapse navbar-collapse">
+        <ul className="nav navbar-nav">
+          <li><a href="#menu">Background</a></li>
+          <li><a href="#background">Work</a></li>
+          <li><a href="#etc">Etc.</a></li>
+          <li><a href="../../Maria_Schreiber_Resume.pdf" download>Resume</a></li>
+          <li><a href="#contact">Contact Me</a></li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="topbar">
+            <a className="topIcon" href="https://github.com/meschreiber" target="_blank">
+              <i className="fa fa-github fa-2x" />
+            </a>
+          </li>
+          <li className="topbar">
+            <a className="topIcon" href="https://www.linkedin.com/in/maria-elisabeth-schreiber/" target="_blank">
+              <i className="fa fa-linkedin fa-2x" />
+            </a>
+          </li>
+          <li className="topbar">
+            <a className="topIcon" href="https://medium.com/@meschreiber3" target="_blank">
+              <i className="fa fa-medium fa-2x" />
+            </a>
+          </li>
+          <li className="topbar">
+            <a className="topIcon" href="mailto:meschreiber3@gmail.com">
+              <i className="fa fa-envelope-o fa-2x" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
+
 )
 
 export default Topbar
