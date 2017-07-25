69c2372be7317f10bab949e614aff49101cf7875
Maria Schreiber <meschreiber3@gmail.com>
2017-07-25 10:32:30 -0400

    Fix typo

--- browser/js/components/Topbar.jsx
+++ browser/js/components/Topbar.jsx
@@ -14,7 +14,7 @@ const Topbar = () => (
           <span className="icon-bar" />
           <span className="icon-bar" />
         </button>
-        <a className="navbar-brand" href="#">Maria Schreiber || Progammer</a>
+        <a className="navbar-brand" href="#">Maria Schreiber || Programmer</a>
       </div>
       <div className="collapse navbar-collapse">
         <ul className="nav navbar-nav">
